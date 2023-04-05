334cab095a2e2e7da716b91b0469283b4f40a854
angelita1005 <114124620+angelita1005@users.noreply.github.com>
2023-04-05 23:04:14 +0800

    Add files via upload

---
+++ Posttest4/src/posttesstt5/src/posttesstt5/karnivora.java
@@ -0,0 +1,51 @@
+package posttesstt5;
+
+public class karnivora extends data {
+
+    private String jenishewan;
+    private String jumlah_kaki;
+    
+    public karnivora(String nama, String tanggalAdopsi, int tinggi, int berat, String jenishewan, String jumlah_kaki){
+    super(nama, tanggalAdopsi, tinggi, berat);
+    this.jenishewan = jenishewan;
+    this.jumlah_kaki = jumlah_kaki;
+        
+    }
+
+
+    public String getJenishewan() {
+        return jenishewan;
+    }
+
+
+    public void setJenishewan(String jenishewan) {
+        this.jenishewan = jenishewan;
+    }
+
+
+    public String getJumlah_kaki() {
+        return jumlah_kaki;
+    }
+
+
+    public void setJumlah_kaki(String jumlah_kaki) {
+        this.jumlah_kaki = jumlah_kaki;
+    }
+    
+    @Override
+    public void look(){
+        System.out.println("Lokasi         : " + lokasi);
+        System.out.println("Nama           : " + getNama());
+        System.out.println("Tanggal Adopsi : " + getTanggalAdopsi());
+        System.out.println("Tinggi         : " + getTinggi());
+        System.out.println("Berat          : " + getBerat());
+        System.out.println("Jenis Hewan    : " + jenishewan);
+        System.out.println("Jumlah Kaki    : " + jumlah_kaki);
+    }
+
+    void add(karnivora karni) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from 
+    }
+    
+
+}
